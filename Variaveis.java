b998e12aa8842348f4564a8b527b81d44709cfc0
danielsbp <danielbatistaodp2@gmail.com>
2021-08-12 19:35:06 +0000

    Adicionando os exercícios

---
+++ Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+    public static void main(String[] args){
+        byte idade = 19;
+        int habitantes = 80000;
+        double salario = 1100;
+        String nome = "Daniel";
+        boolean semaforoAberto = true;
+        long celulares = 100000L;
+        char genero = 'M';
+    }
+}
