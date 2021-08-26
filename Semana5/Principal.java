529d38660cc2b08033e4600af3e12fdd04c79080
danielsbp <danielbatistaodp2@gmail.com>
2021-08-26 20:35:09 +0000

    Adicionando exercícios da semana 5

---
+++ Semana5/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String [] args) {
+       
+       Fatorial fatorial = new Fatorial();
+       fatorial.iniciar();
+       
+       Condicional condicional = new Condicional();
+       condicional.iniciar();
+
+       Tabela tabela = new Tabela();
+       tabela.iniciar();
+
+    }
+}
