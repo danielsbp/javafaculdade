ac41209448f05ff3baa110d94ebf37c26c3eb25f
danielsbp <danielbatistaodp2@gmail.com>
2021-09-02 16:52:16 +0000

    Fazendo alterações na classe principal

--- Semana5/Principal.java
+++ Semana5/Principal.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
 public class Principal {
     public static void main(String [] args) {
-       
-       Fatorial fatorial = new Fatorial();
-       fatorial.iniciar();
-       
-       Condicional condicional = new Condicional();
-       condicional.iniciar();
-
-       Tabela tabela = new Tabela();
-       tabela.iniciar();
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Qual método executar?");
+       System.out.println("1 - Fatorial");
+       System.out.println("2 - Condicional");
+       System.out.println("3 - Tabela");
+       int n = sc.nextInt();
+       if(n==1){
+           Fatorial fatorial = new Fatorial();
+           fatorial.iniciar();
+       }
+       else if(n==2) {
+           Condicional condicional = new Condicional();
+           condicional.iniciar();
+       }
+       else if(n==3) {
+           Tabela tabela = new Tabela();
+           tabela.iniciar();
+       }
+       else {
+           System.out.println("Número digitado inválido!");
+       }
 
     }
 }
