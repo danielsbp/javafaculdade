d9a066fe8f11b433dce6068c7c6b52d5745775c4
danielsbp <danielbatistaodp3@gmail.com>
2021-11-04 20:00:46 +0000

    Arquivos da semana 13

---
+++ Semana13/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+}
