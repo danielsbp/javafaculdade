8b527712d61d49e1be99224ee97ac450bdcf2867
danielsbp <danielbatistaodp3@gmail.com>
2022-02-22 17:50:25 +0000

    Correção 2

---
+++ Java II/RevisãoHerança/Empregado.java
@@ -0,0 +1,10 @@
+public class Empregado {
+    private double salario;
+    
+    public double getSalario() {
+        return this.salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+} 
